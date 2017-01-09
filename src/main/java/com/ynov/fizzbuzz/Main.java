6d098a325010e3945f68c7327d8dbf0c0c489776
rbeneteau <r.beneteau@outlook.fr>
2017-01-10 00:10:50 +0100

    Display numbers from 1 to 100.

--- src/main/java/com/ynov/fizzbuzz/Main.java
+++ src/main/java/com/ynov/fizzbuzz/Main.java
@@ -3,7 +3,11 @@ package com.ynov.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-        // Empty entry point...
+        // On parcours les nombre de 1 à 100
+        // Le nombre courant sera stocké dans la variable primitive de type INT 'i'
+        for(int i = 1; i <= 100; i++) {
+            System.out.print(i+" ");
+        }
     }
 
 }
